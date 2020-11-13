e7bc3cd5ba66c23d848b42443bd27a9defdf76a4
OneMoreTry1 <youfeelmyskill1@gmail.com>
2020-11-13 15:52:27 +0200

    login auth

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from './Header.module.css';
 
-const Header = () => {
+const Header = (props) => {
 	return (
 		<header className={classes.header}>
 			<img src='https://png.pngtree.com/element_pic/00/16/07/115783931601b5c.jpg' alt='Logo' />
+
+			<div className={classes.loginBlock}>
+				{props.isAuth ? props.login
+					: <NavLink to={'/login'}>Login</NavLink>}
+			</div>
 		</header>
 	);
 }
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,6 +1,7 @@
 import * as axios from 'axios';
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import { setUserProfile } from '../../redux/profile-reducer';
 import Profile from './Profile';
 
@@ -8,7 +9,11 @@ import Profile from './Profile';
 class ProfileContainer extends React.Component {
 
 	componentDidMount() {
-		axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`).then(response => {
+		let userId = this.props.match.params.userId;
+		if (!userId) {
+			userId = 2;
+		}
+		axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId).then(response => {
 			this.props.setUserProfile(response.data);
 		});
 	}
@@ -24,4 +29,6 @@ let mapStateToProps = (state) => ({
 	profile: state.profilePage.profile
 });
 
-export default connect(mapStateToProps, { setUserProfile })(ProfileContainer);
+let WithUrlDataContainerComponent = withRouter(ProfileContainer);
+
+export default connect(mapStateToProps, { setUserProfile })(WithUrlDataContainerComponent);
